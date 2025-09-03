b004c8400805dfde97bcd4b51b97d0b146e38821
Anilkumar0017 <eete.kumar@iitgn.ac.in>
2025-09-03 15:57:39 +0530

    Initial commit with pylint workflow

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 # STT-LAB1
+=======
+# STT-LAB1 Project
+>>>>>>> 498e770 (Initial commit with pylint workflow)
---
+++ hello.py
@@ -0,0 +1,9 @@
+"""Sample hello script for pylint test.""" 
+
+def main():
+    """Main function."""
+    print("Hello, GitHub Actions!")
+
+if __name__ == "__main__":
+    main()
+
